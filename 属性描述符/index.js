0fa12c9667b3d728c4922e26bd1de8947b8527ca
kingnan <373734199@qq.com>
2023-05-23 00:42:32 +0800

    属性描述符相关

---
+++ 属性描述符/index.js
@@ -0,0 +1,37 @@
+console.log(Object.prototype.__proto_ == null);
+
+
+var obj = Object.create(Object.prototype, {
+    _x:{
+        value:1,
+        writable:true,
+    },
+    x:{
+        get:function() {
+            return this._x;
+        },
+        set:function (value) {
+            if (typeof value !== 'number') {
+                throw new Error('input number')
+            }
+            this.__x = value
+        }
+    }
+})
+
+var des = Object.getOwnPropertyDescriptor(obj, 'x')
+des.set= function (value) {
+    if (typeof value !== 'number' && isNaN(value * 1)) {
+        throw new Error('input number')
+    }
+    this.__x = value
+}
+console.log(obj.x);
+console.log(des);
+ Object.defineProperty(obj, "x", des)
+// obj.x = 2
+console.log(obj);
+
+// var obj2 = Object.create({});
+// obj2 = Object.defineProperty(obj2, "foo", {value: "bar"}); 
+// console.log(obj2);
---
+++ 属性描述符/index2.js
@@ -0,0 +1,47 @@
+function extend(toObject, formObject) {
+    for (const prototype in formObject) {
+        if(!formObject.hasOwnProperty(prototype)){
+            continue
+        }
+        Object.defineProperty(toObject,prototype,Object.getOwnPropertyDescriptor(formObject, prototype))
+    }
+    return toObject
+}
+
+
+var obj = {}
+obj.x = 1
+obj = extend(obj, {get y(){ return 2}})
+console.log(obj.y);
+
+
+var obj2 = {}
+Object.defineProperty(obj2, 'x', {
+    value:175,
+    writable:false,
+    enumerable:true,
+    configurable: true
+})
+obj = extend(obj, obj2)
+
+console.log(obj.x);
+obj.x = 90
+console.log(obj.x); //writable:false,
+
+
+
+
+console.log(Object.isExtensible(obj));
+Object.preventExtensions(obj) //阻止添加新属性
+console.log(Object.isExtensible(obj));
+
+var obj3 = {}
+console.log(Object.isSealed(obj3));
+Object.seal(obj3)//阻止添加新属性， 无法删除旧属性  === configurable: false
+console.log(Object.isSealed(obj3));
+
+
+var obj4 = {}
+console.log(Object.isFrozen(obj4));
+Object.isFrozen(obj4)//阻止添加新属性。 删除旧对象， 修改属性值
+console.log(Object.isFrozen(obj4));
